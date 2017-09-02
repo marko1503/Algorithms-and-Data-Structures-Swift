20da8a6fac1b28dfb4aad4657fb41546f5ae6471
Maksym Prokopchuk <rowwingman@gmail.com>
2017-09-03 02:07:06 +0300

    Fix insertion algorithm

--- Sort/Insertion Sort/InsertionSort.playground/Pages/Version_1.xcplaygroundpage/Contents.swift
+++ Sort/Insertion Sort/InsertionSort.playground/Pages/Version_1.xcplaygroundpage/Contents.swift
@@ -1,8 +1,38 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
-// MARK: - Insertion sort
+// MARK: - Insertion sort algorithm
+//: Comlexity: O(n^2), Storage: 2N
+//: ![Sort example](Insertion_sort_example_n_storage.pdf)
+
+func insertionSort<Element>(array: [Element]) -> [Element] where Element: Comparable {
+    // check for tirivial case
+    // we have only 1 element in array
+    guard array.count > 1 else { return array }
+
+    var output: [Element] = array
+    let elementsCount = output.count
+
+    for primaryIndex in 0..<elementsCount {
+        let primaryElement = output[primaryIndex]
+        var tempIndex = primaryIndex - 1
+
+        while tempIndex > -1 && output[tempIndex] > primaryElement {
+            // primaryElement and element at tempIndex are not ordered. We need to change them
+            // Then, remove primary element
+            // tempIndex + 1 - primaryElement always after tempIndex
+            output.remove(at: tempIndex + 1)
+            // and insert primary element before tempElement
+            output.insert(primaryElement, at: tempIndex)
+            tempIndex -= 1
+        }
+    }
+
+    return output
+}
+
+
 //func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
 //    var a = array
 //    for x in 1..<a.count {
@@ -17,32 +47,26 @@ import UIKit
 //    return a
 //}
 
-//: Comlexity: O(n^2), Storage: 2N
-//: ![Sort example](Insertion_sort_example_2n_storage.pdf)
+//let array = [Int]()
+//insertionSort(array: array)
 
-func insertionSort<Element>(array: [Element]) -> [Element] {
-    // check for tirivial case
-    // we have only 1 element in array
-    guard array.count > 1 else { return array }
+//let array = [5]
+//insertionSort(array: array)
 
-    var output: [Element] = array
+//let array = [5, 8]
+//insertionSort(array: array)
 
-    for primaryIndex in 1..<output.count {
-        let primaryElement = output[primaryIndex]
-        var tempIndex = primaryIndex - 1
+//let array = [8, 5]
+//insertionSort(array: array)
 
-        while tempIndex > -1 {
-            // primaryElement and element at tempIndex are not ordered. We need to change them
-            // Then, remove primary element
-            // tempIndex + 1 - primaryElement always after tempIndex
-            output.remove(at: tempIndex + 1)
-            // and insert primary element before tempElement
-            output.insert(primaryElement, at: tempIndex)
-            tempIndex -= 1
-        }
-    }
+//let array = [1, 2, 3, 4, 5, 6, 7, 8]
+//insertionSort(array: array)
 
-    return output
-}
+//let array = [8, 7, 6, 5, 4, 3, 2, 1]
+//insertionSort(array: array)
+
+let array = [7, 2, 1, 6, 8, 5, 3, 4]
+insertionSort(array: array)
+
 
 //: [Next](@next)
--- Sort/Insertion Sort/InsertionSort.playground/Pages/Version_3.xcplaygroundpage/Contents.swift
+++ Sort/Insertion Sort/InsertionSort.playground/Pages/Version_3.xcplaygroundpage/Contents.swift
@@ -2,29 +2,50 @@
 
 import Foundation
 
-//public func insertionSort(sortDirection: (Element, Element) -> Bool) -> [Element] {
-//    // check for tirivial case
-//    // we have only 1 element in array
-//    guard self.count > 1 else { return self }
-//
-//    var output: [Element] = self
-//
-//    for primaryIndex in 1..<output.count {
-//        let primaryElement = output[primaryIndex]
-//        var tempIndex = primaryIndex - 1
-//
-//        while tempIndex > -1 && sortDirection(primaryElement, output[tempIndex]) {
-//            // primaryElement and element at tempIndex are not ordered. We need to change them
-//            // Then, remove primary element
-//            // tempIndex + 1 - primaryElement always after tempIndex
-//            output.remove(at: tempIndex + 1)
-//            // and insert primary element before tempElement
-//            output.insert(primaryElement, at: tempIndex)
-//            tempIndex -= 1
-//        }
-//    }
-//
-//    return output
-//}
+func insertionSort<Element>(array: [Element], sortDirection: (Element, Element) -> Bool) -> [Element] where Element: Comparable {
+    // check for tirivial case
+    // we have only 1 element in array
+    guard array.count > 1 else { return array }
+
+    var output: [Element] = array
+
+    for primaryIndex in 1..<output.count {
+        let primaryElement = output[primaryIndex]
+        var tempIndex = primaryIndex - 1
+
+        while tempIndex > -1 && sortDirection(primaryElement, output[tempIndex]) {
+            // primaryElement and element at tempIndex are not ordered. We need to change them
+            // Then, remove primary element
+            // tempIndex + 1 - primaryElement always after tempIndex
+            output.remove(at: tempIndex + 1)
+            // and insert primary element before tempElement
+            output.insert(primaryElement, at: tempIndex)
+            tempIndex -= 1
+        }
+    }
+
+    return output
+}
+
+//let array = [Int]()
+//insertionSort(array: array)
+
+//let array = [5]
+//insertionSort(array: array)
+
+//let array = [5, 8]
+//insertionSort(array: array)
+
+//let array = [8, 5]
+//insertionSort(array: array)
+
+//let array = [1, 2, 3, 4, 5, 6, 7, 8]
+//insertionSort(array: array)
+
+//let array = [8, 7, 6, 5, 4, 3, 2, 1]
+//insertionSort(array: array)
+
+let array = [7, 2, 1, 6, 8, 5, 3, 4]
+insertionSort(array: array, sortDirection: <)
 
 //: [Next](@next)
